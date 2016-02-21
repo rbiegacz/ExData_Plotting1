259a8cb4758b911993a23d03aed186d6426e080a
Rafal Biegacz <rafal.biegacz@intel.com>
2016-02-21 21:31:52 +0100

    First version of the plot1.R that produces the desired plot
    First version of the plot1.R that produces the desired plot. Plot is
    saved in png file.

--- plot1.R
+++ plot1.R
@@ -1,1 +1,16 @@
-read.csv("household_power_consumption.txt")
+# This command reads in the data from the file
+whole_data <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";")
+
+# This command chooses rows that are younger than 2007 Jan 31 which means data from 2007 Feb 1 and later
+data1 <- subset(whole_data, strptime(V1, "%d/%m/%C%y") > strftime("2007/01/31", "%C%y-%m-%d"))
+# This command chooses rows that are older than 2007 Feb 3 which now gives us data starting from 2007 Feb 1 and ending on 2007 Feb 2 
+data2 <- subset(data1, strptime(V1, "%d/%m/%C%y") < strftime("2007/02/3", "%C%y-%m-%d"))
+
+# OK, let's plot histogram for "Global Active Power" data
+hist(as.numeric(paste(data2$V3)), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+# saving the plot to plot1.png file
+dev.copy(png,'plot1.png')
+
+# closign the file
+dev.off()
