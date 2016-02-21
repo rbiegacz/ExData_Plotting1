bfd9f8bba101851c2f405176702915d80e021dc2
Rafal Biegacz <rafal.biegacz@intel.com>
2016-02-21 23:10:00 +0100

    almost final version of plot4 script
    almost final version of plot4 script - all the plots are done, the graph
    saved to a plot4.png file

--- plot4.R
+++ plot4.R
@@ -1,1 +1,50 @@
-read.csv("household_power_consumption.txt")
+# This command reads in the data from the file
+whole_data <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";")
+
+# This command chooses rows that are younger than 2007 Jan 31 which means data from 2007 Feb 1 and later
+data1 <- subset(whole_data, strptime(V1, "%d/%m/%C%y") > strftime("2007/01/31", "%C%y-%m-%d"))
+# This command chooses rows that are older than 2007 Feb 3 which now gives us data starting from 2007 Feb 1 and ending on 2007 Feb 2 
+data2 <- subset(data1, strptime(V1, "%d/%m/%C%y") < strftime("2007/02/3", "%C%y-%m-%d"))
+
+# making margins a little bit smaller
+#par(mar = c(3,4,2,2))
+
+# configuring two columns and two rows in the plot area
+par(mfcol = c(2,2), mfrow = c(2, 2))
+
+# Drawing the first graph
+plot(paste(data2$V3), type = "l", ylab = "Global Active Power (kilowatts)", xaxt = "n")
+x <- c("Thu", "Fri", "Sat")
+xx <- c(1, nrow(data2)/2, nrow(data2))
+axis(1, at = xx, labels=x)
+
+# Drawing the second graph
+plot(paste(data2$V5), type = "l", ylab = "Voltage", xlab = "datetime")
+x <- c("Thu", "Fri", "Sat")
+xx <- c(1, nrow(data2)/2, nrow(data2))
+axis(1, at = xx, labels=x)
+
+# OK, let's plot histogram for "Global Active Power" data
+plot(paste(data2$V7), type = "l", ylab = "Energy sub metering", xaxt = "n", col = "black")
+points(paste(data2$V8), type = "l", xaxt = "n", col = "red")
+points(paste(data2$V9), type = "l", xaxt = "n", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = "-", col=c("black", "red", "blue"), lwd = 2)
+
+# setting the labels/ticks on X axis
+x <- c("Thu", "Fri", "Sat")
+xx <- c(1, nrow(data2)/2, nrow(data2))
+axis(1, at = xx, labels=x)
+
+# Drawing the forth graph
+plot(paste(data2$V4), type = "l", ylab = "Global reactive power", xlab = "datetime")
+x <- c("Thu", "Fri", "Sat")
+xx <- c(1, nrow(data2)/2, nrow(data2))
+axis(1, at = xx, labels=x)
+
+
+# saving the plot to plot1.png file
+dev.copy(device = png,'plot4.png', width = 480, height = 480)
+
+# closign the file
+dev.off()
+
