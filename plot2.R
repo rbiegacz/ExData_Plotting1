f5f051d9704b712343acf5c608e0844eb35d074b
Rafal Biegacz <rafal.biegacz@intel.com>
2016-02-21 23:00:05 +0100

    correcting bottom margin, adding legend to plot3
    correcting bottom margin, adding legend to plot3

--- plot2.R
+++ plot2.R
@@ -6,9 +6,10 @@ data1 <- subset(whole_data, strptime(V1, "%d/%m/%C%y") > strftime("2007/01/31", 
 # This command chooses rows that are older than 2007 Feb 3 which now gives us data starting from 2007 Feb 1 and ending on 2007 Feb 2 
 data2 <- subset(data1, strptime(V1, "%d/%m/%C%y") < strftime("2007/02/3", "%C%y-%m-%d"))
 
-# OK, let's plot histogram for "Global Active Power" data
-#hist(as.numeric(paste(data2$V3)), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+# making margins a little bit smaller
+par(mar = c(3,4,2,2))
 
+# OK, let's plot histogram for "Global Active Power" data
 plot(paste(data2$V3), type = "l", ylab = "Global Active Power (kilowatts)", xaxt = "n")
 x <- c("Thu", "Fri", "Sat")
 xx <- c(1, nrow(data2)/2, nrow(data2))
--- plot3.R
+++ plot3.R
@@ -6,13 +6,16 @@ data1 <- subset(whole_data, strptime(V1, "%d/%m/%C%y") > strftime("2007/01/31", 
 # This command chooses rows that are older than 2007 Feb 3 which now gives us data starting from 2007 Feb 1 and ending on 2007 Feb 2 
 data2 <- subset(data1, strptime(V1, "%d/%m/%C%y") < strftime("2007/02/3", "%C%y-%m-%d"))
 
+# making margins a little bit smaller
+par(mar = c(3,4,2,2))
+
 # OK, let's plot histogram for "Global Active Power" data
-#hist(as.numeric(paste(data2$V3)), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 plot(paste(data2$V7), type = "l", ylab = "Energy sub metering", xaxt = "n", col = "black")
 points(paste(data2$V8), type = "l", xaxt = "n", col = "red")
 points(paste(data2$V9), type = "l", xaxt = "n", col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = "-", col=c("black", "red", "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = "-", col=c("black", "red", "blue"), lwd = 2)
 
+# setting the labels/ticks on X axis
 x <- c("Thu", "Fri", "Sat")
 xx <- c(1, nrow(data2)/2, nrow(data2))
 axis(1, at = xx, labels=x)
