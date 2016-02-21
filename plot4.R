526fc5fed74374158e57ba676ab8b35f1ffac7fa
Rafal Biegacz <rafal.biegacz@intel.com>
2016-02-21 23:20:42 +0100

    titles under plots were corrected, scaling of graphs corrected
    titles under plots were corrected, scaling of graphs corrected

--- plot4.R
+++ plot4.R
@@ -6,14 +6,15 @@ data1 <- subset(whole_data, strptime(V1, "%d/%m/%C%y") > strftime("2007/01/31", 
 # This command chooses rows that are older than 2007 Feb 3 which now gives us data starting from 2007 Feb 1 and ending on 2007 Feb 2 
 data2 <- subset(data1, strptime(V1, "%d/%m/%C%y") < strftime("2007/02/3", "%C%y-%m-%d"))
 
+
 # making margins a little bit smaller
-#par(mar = c(3,4,2,2))
+par(mar = c(4,4,1,1))
 
 # configuring two columns and two rows in the plot area
-par(mfcol = c(2,2), mfrow = c(2, 2))
+par(mfrow = c(2, 2))
 
 # Drawing the first graph
-plot(paste(data2$V3), type = "l", ylab = "Global Active Power (kilowatts)", xaxt = "n")
+plot(paste(data2$V3), type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 x <- c("Thu", "Fri", "Sat")
 xx <- c(1, nrow(data2)/2, nrow(data2))
 axis(1, at = xx, labels=x)
@@ -24,8 +25,9 @@ x <- c("Thu", "Fri", "Sat")
 xx <- c(1, nrow(data2)/2, nrow(data2))
 axis(1, at = xx, labels=x)
 
+# Drawing the third graph
 # OK, let's plot histogram for "Global Active Power" data
-plot(paste(data2$V7), type = "l", ylab = "Energy sub metering", xaxt = "n", col = "black")
+plot(paste(data2$V7), type = "l", ylab = "Energy sub metering", xlab = "", col = "black")
 points(paste(data2$V8), type = "l", xaxt = "n", col = "red")
 points(paste(data2$V9), type = "l", xaxt = "n", col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = "-", col=c("black", "red", "blue"), lwd = 2)
